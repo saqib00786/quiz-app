e16739267f462fa0e6644df4b3e82b58e912e615
Muhammad Saqib <37036366+saqib00786@users.noreply.github.com>
2022-12-02 17:10:25 +0500

    update # 01

---
+++ navigation/index.js
@@ -0,0 +1,34 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NavigationContainer } from '@react-navigation/native'
+import Result from '../screens/Result'
+import DashBoard from '../screens/DashBoard'
+
+
+const Stack = createNativeStackNavigator()
+
+const Navigation = () => {
+
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name={'Result'}
+                    component={Result}
+                    options={{}}
+                />
+                <Stack.Screen
+                    name={'DashBoard'}
+                    component={DashBoard}
+                    options={{}}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+
+    )
+}
+
+export default Navigation
+
+const styles = StyleSheet.create({})
